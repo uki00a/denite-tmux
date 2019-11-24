0db4d682eaad3c10669ee7a2744b19bc8bf4ef7b
uki00a <uki00a@gmail.com>
2019-11-24 17:49:09 +0900

    refactor: use function instead of method

--- rplugin/python3/denite/source/tmux/session.py
+++ rplugin/python3/denite/source/tmux/session.py
@@ -3,13 +3,24 @@ import re
 
 from ..base import Base as BaseSource
 from ...kind.base import Base as BaseKind
-from denite.util import debug
 
 # TODO error handling
 # TODO refactor
 
 def _run_command(args):
     return subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+def _parse_output(output):
+    return [_parse_line(line) for line in output.split('\n') if not re.match(r"^\s$", line)]
+
+
+def _parse_line(line):
+    session_name = re.split(r'\s+', line, maxsplit=1)[0]
+    return {
+        'word': session_name,
+        'abbr': line,
+        'action__session': session_name
+    }
 
 class Source(BaseSource):
     def __init__(self, vim):
@@ -20,18 +31,7 @@ class Source(BaseSource):
     def gather_candidates(self, context):
         process = _run_command(['tmux', 'list-sessions', '-F', '#{session_name} #{?session_attached,(attached),}'])
         output = process.stdout.decode('utf-8')
-        return self._parse_output(output)
-
-    def _parse_output(self, output):
-        return [self._parse_line(line) for line in output.split('\n') if not re.match(r"^\s$", line)]
-
-    def _parse_line(self, line):
-        session_name = re.split(r'\s+', line, maxsplit=1)[0]
-        return {
-            'word': session_name,
-            'abbr': line,
-            'action__session': session_name
-        }
+        return _parse_output(output)
 
 class TmuxSession(BaseKind):
     def __init__(self, vim):
