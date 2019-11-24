60032f393584c4ddc1b8d1eb96d7e36eb615e635
uki00a <uki00a@gmail.com>
2019-11-24 17:28:35 +0900

    update(tmux/session): abbr format

--- rplugin/python3/denite/source/tmux/session.py
+++ rplugin/python3/denite/source/tmux/session.py
@@ -18,7 +18,7 @@ class Source(BaseSource):
         self.kind = TmuxSession(vim)
 
     def gather_candidates(self, context):
-        process = _run_command(['tmux', 'list-sessions'])
+        process = _run_command(['tmux', 'list-sessions', '-F', '#{session_name} #{?session_attached,(attached),}'])
         output = process.stdout.decode('utf-8')
         return self._parse_output(output)
 
@@ -26,11 +26,10 @@ class Source(BaseSource):
         return [self._parse_line(line) for line in output.split('\n') if not re.match(r"^\s$", line)]
 
     def _parse_line(self, line):
-        values = re.split(r'\s+', line)
-        session_name = values[0]
+        session_name = re.split(r'\s+', line, maxsplit=1)[0]
         return {
             'word': session_name,
-            'abbr': session_name,
+            'abbr': line,
             'action__session': session_name
         }
 
